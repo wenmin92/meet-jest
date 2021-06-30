94212cd6943869a66a02b35352115ac7d9bd8584
wenmin92 <wenmin92@gmail.com>
2021-06-30 18:15:25 +0800

    init

---
+++ global-bottom.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <div
+      class="absolute bottom-2 left-3"
+      v-if="$slidev.nav.currentLayout !== 'cover'"
+    >
+      {{ $slidev.nav.currentPage - 1 }} / {{ $slidev.nav.total - 1 }}
+    </div>
+    <div class="absolute bottom-2.5 right-2">
+      <img class="w-24" src="/logo.png" />
+    </div>
+  </footer>
+</template>
